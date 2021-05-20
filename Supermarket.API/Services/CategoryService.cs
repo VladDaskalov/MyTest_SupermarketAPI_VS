ef01d4c837f32541d088ad504f8521472eb05d18
Vladislav Daskalov <vladislav.das@gmail.com>
2021-05-20 15:57:58 +0300

    1. Product Service class added 2. Category service updated to use the renamed "CategoryResponse" class

--- Supermarket.API/Services/CategoryService.cs
+++ Supermarket.API/Services/CategoryService.cs
@@ -8,34 +8,81 @@ using System;
 
 namespace Supermarket.API.Services
 {
-    public class CategoryService : ICategoryService
-    {
-        private readonly ICategoryRepository _categoryRepository;
+	public class CategoryService : ICategoryService
+	{
+		private readonly ICategoryRepository _categoryRepository;
 		private readonly IUnitOfWork _unitOfWork;
 
-        public CategoryService(ICategoryRepository categoryRepository)
-        {
-            _categoryRepository = categoryRepository;
-        }
+		public CategoryService(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+		{
+			_categoryRepository = categoryRepository;
+			_unitOfWork = unitOfWork;
+		}
 
-        public async Task<IEnumerable<Category>> ListAsync()
-        {
-            return await _categoryRepository.ListAsync();
-        }
+		public async Task<IEnumerable<Category>> ListAsync()
+		{
+			return await _categoryRepository.ListAsync();
+		}
 
-		public async Task<SaveCategoryResponse> SaveAsync(Category category)
+		public async Task<CategoryResponse> SaveAsync(Category category)
 		{
 			try
 			{
 				await _categoryRepository.AddAsync(category);
 				await _unitOfWork.CompleteAsync();
 
-				return new SaveCategoryResponse(category);
+				return new CategoryResponse(category);
 			}
 			catch (Exception ex)
 			{
 				// Do some logging stuff
-				return new SaveCategoryResponse($"An error occurred when saving the category: {ex.Message}");
+				return new CategoryResponse($"An error occurred when saving the category: {ex.Message}");
+			}
+		}
+
+		public async Task<CategoryResponse> UpdateAsync(int id, Category category)
+		{
+			var existingCategory = await _categoryRepository.FindByIdAsync(id);
+
+			if (existingCategory == null)
+			{
+				return new CategoryResponse("Category not found");
+			}
+
+			existingCategory.Name = category.Name;
+
+			try
+			{
+				_categoryRepository.Update(existingCategory);
+				await _unitOfWork.CompleteAsync();
+				return new CategoryResponse(existingCategory);
+			}
+			catch (Exception ex)
+			{
+				// Do some logging stuff
+				return new CategoryResponse($"An error occurred when updating the category: {ex.Message}");
+			}
+		}
+
+		public async Task<CategoryResponse> DeleteAsync(int id)
+		{
+			var existingCategory = await _categoryRepository.FindByIdAsync(id);
+
+			if (existingCategory == null)
+			{
+				return new CategoryResponse("Category not found");
+			}
+
+			try
+			{
+				_categoryRepository.Remove(existingCategory);
+				await _unitOfWork.CompleteAsync();
+				return new CategoryResponse($"The following category was deleted - ID: {existingCategory.Id}");
+			}
+			catch (Exception ex)
+			{
+				// Do some logging stuff
+				return new CategoryResponse($"An error occurred when deleting the category: {ex.Message}");
 			}
 		}
 	}
---
+++ Supermarket.API/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Supermarket.API.Domain.Services;
+using Supermarket.API.Domain.Repositories;
+using Supermarket.API.Domain.Models;
+
+namespace Supermarket.API.Services
+{
+    public class ProductService: IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IUnitOfWork unitOfWork, IProductRepository productRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> ListAsync()
+        {
+            return await _productRepository.ListAsync();
+        }
+    }
+}
