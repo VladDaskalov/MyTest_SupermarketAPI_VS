c593ea805e7e618727523808a7825605aa62b9d9
Vladislav Daskalov <vladislav.das@gmail.com>
2021-05-20 15:54:37 +0300

    Updated Controller files - new methods added for Categories and Products handling

---
+++ Supermarket.API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using System.Collections.Generic;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Resources;
+using Supermarket.API.Domain.Services;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Controllers
+{
+    [Route("/api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProductResource>> GetAllAsync()
+        {
+            var products = await _productService.ListAsync();
+            var resources = _mapper.Map<IEnumerable<Product>, IEnumerable<ProductResource>>(products);
+            return resources;
+        }
+
+    }
+}
